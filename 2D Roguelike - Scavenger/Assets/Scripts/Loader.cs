0499882ac126088fabdfbcf5b6f48402742ac58f
unknown <thienphucdoantran@gmail.com>
2023-06-15 13:32:23 +0700

    Scavenger - Player animation update

---
+++ 2D Roguelike - Scavenger/Assets/Scripts/Loader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject gameManager;
+    void Awake()
+    {
+        if(GameManager.instance == null){
+            Instantiate(gameManager);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
