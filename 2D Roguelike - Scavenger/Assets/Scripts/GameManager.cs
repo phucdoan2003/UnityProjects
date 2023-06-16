17e5b9e1569bb13d108f913bf40d52d1107eb834
unknown <thienphucdoantran@gmail.com>
2023-06-16 13:07:31 +0700

    2d roguelike scavenger

--- 2D Roguelike - Scavenger/Assets/Scripts/GameManager.cs
+++ 2D Roguelike - Scavenger/Assets/Scripts/GameManager.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float turnDelay = .1f;
     public static GameManager instance = null;
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playerTurn = true;
     
     private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
     // Start is called before the first frame update
     void Awake()
     {
@@ -17,17 +22,45 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
         DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
 
     void InitGame(){
+        enemies.Clear();
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver(){
+        enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerTurn || enemiesMoving){
+            return;
+        }
+
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy script){
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies(){
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if(enemies.Count == 0){
+            yield return new WaitForSeconds(turnDelay);
+        }
+        for(int i = 0; i < enemies.Count; i++){
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playerTurn = true;
     }
 }
